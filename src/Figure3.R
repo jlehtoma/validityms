b781d7c0b9ba76f40f88d78cde4dcf9ebaca19f7
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-03-14 18:32:35 +0200

    Add boxplotting for particular fractions of the landscape

--- src/Figure3.R
+++ src/Figure3.R
@@ -8,6 +8,34 @@ spp.labels <- c("1"="Birch", "2"="Spruce", "3"="OtherDec", "4"="Pine")
 fert.labels <- c("1"="Lehto", "2"="Lehtomainen", "3"="Tuore", "4"="Kuivahko", 
                  "5"="Kuiva")
 
+
+# Variant1 ----------------------------------------------------------------
+
+# Get variant 1 (3): 16_60_5kp_abf_pe_w
+variant.1 <- get_variant(project.esmk, 3)
+featurenames(variant.1) <- fix_feature_names(featurenames(variant.1))
+groupnames(variant.1) <- spp.labels
+
+## Performance curves by tree species
+
+grp.curves.1 <- curves(variant.1, groups=TRUE)
+p1 <- plot(grp.curves.1, monochrome=FALSE, 
+           invert.x=FALSE, main="Tree species group")
+
+## Boxplots by fertility class 
+# Take values [0.8, 1.0], leave everything else but pr_lost out
+bp.curves.1 <- curves(variant.1, lost.lower=0.8, lost.upper=1.0, cols=c(1,8:27))
+
+# [todo] - needs to refactored to a proper function
+m.bp.curves.1 <- melt(bp.curves.1, id.vars = c("pr_lost"))
+m.bp.curves.1$tree.spp <- sapply(strsplit(as.character(m.bp.curves.1$variable), 
+                                          "_"), function(x){x[1]})
+m.bp.curves.1$soil.fert <- sapply(strsplit(as.character(m.bp.curves.1$variable), 
+                                           "_"), function(x){x[2]})
+p <- ggplot(m.bp.curves.1, aes(factor(soil.fert), value))
+p + geom_boxplot()
+
+
 # Get variant 4: 17_60_5kp_abf_pe_w_cmat
 variant.4 <- get_variant(project.esmk, 4)
 groupnames(variant.4) <- spp.labels
