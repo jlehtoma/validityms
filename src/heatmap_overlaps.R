8c97146280ab74c4708808d46814d36bffe229f5
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-03-30 19:10:59 +0300

    Add more heatmap_overlaps

--- src/heatmap_overlaps.R
+++ src/heatmap_overlaps.R
@@ -34,6 +34,19 @@ cross_range_jaccard <- function(raster1, raster2, thresholds, ...) {
 
 m_cross_range_jaccard <- addMemoization(cross_range_jaccard)
 
+classes <- c("0-10", "10-20", "20_30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100")
 thresholds <- seq(0.1, 1, 0.1)
+
 ranks.abf.pe <- rank_rasters(project.esmk, variants=c(2, 9))
-j <- cross_range_jaccard(ranks.abf.pe[[1]], ranks.abf.pe[[2]], thresholds)
+j.classes.ranks.abf.pe <- m_cross_range_jaccard(ranks.abf.pe[[1]], ranks.abf.pe[[2]], thresholds)
+colnames(j.classes.ranks.abf.pe) <- classes
+j.classes.ranks.abf.pe$classes <- classes
+
+ranks.abf.pe.w.cmat <- rank_rasters(project.esmk, variants=c(4, 11))
+j.classes.ranks.abf.pe.w.cmat <- m_cross_range_jaccard(ranks.abf.pe.w.cmat[[1]], ranks.abf.pe.w.cmat[[2]], thresholds)
+colnames(j.classes.ranks.abf.pe.w.cmat) <- classes
+j.classes.ranks.abf.pe.w.cmat$classes <- classes
+
+m.j.classes.ranks.abf.pe <- melt(j.classes.ranks.abf.pe, id.vars=c("classes"))
+p1 <- ggplot(m.j.classes.ranks.abf.pe, aes(x=variable, y=classes))
+p1 + geom_tile(aes(fill = value), colour = "white") + scale_fill_gradient(low = "white", high = "steelblue")
