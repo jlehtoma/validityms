371a42438d91502b65bc6e44a66f5b9f2c657ddc
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-02 14:13:55 +0300

    Update Fig2 src file

--- src/Figure2.R
+++ src/Figure2.R
@@ -5,6 +5,10 @@ load.project()
 
 # Set the colors used to spectral divergent
 z_colors_spectral <- zlegend('spectral')
+# Construct a suitable color key for levelplots
+z_color_key <- list(at=c(z_colors_spectral$values),
+                    labels=list(labels=z_colors_spectral$labels,
+                                at=z_colors_spectral$values))
 
 # MSNFI without sfc classes -----------------------------------------------
 
@@ -13,10 +17,14 @@ rankr.nosfc.msnfi.abf.pe.w.cmat <- rank_raster(nosfc.msnfi.abf.pe.w.cmat)
 #plot(rankr.abf, useRaster=TRUE, interpolate=TRUE, 
 #     breaks=z_colors_spectral$values, col=z_colors_spectral$colors)
 
-p1 <- levelplot(rankr.nosfc.msnfi.abf.pe.w, FUN.margin=median, 
-                par.settings=rasterTheme(region=z_colors_spectral$colors))
-p2 <- levelplot(rankr.nosfc.msnfi.abf.pe.w.cmat, FUN.margin=median, 
-                par.settings=rasterTheme(region=z_colors_spectral$colors))
+p1 <- levelplot(rankr.nosfc.msnfi.abf.pe.w, FUN.margin=median,
+                maxpixels=1e6,
+                par.settings=rasterTheme(region=z_colors_spectral$colors), 
+                at=z_colors_spectral$values, colorkey=z_color_key)
+p2 <- levelplot(rankr.nosfc.msnfi.abf.pe.w.cmat, FUN.margin=median,
+                maxpixels=1e6,
+                par.settings=rasterTheme(region=z_colors_spectral$colors),
+                at=z_colors_spectral$values, colorkey=z_color_key)
 
 # MSNFI with sfc classes --------------------------------------------------
 
@@ -26,9 +34,13 @@ rankr.msnfi.abf.pe.w.cmat <- rank_raster(msnfi.abf.pe.w.cmat)
 #     breaks=z_colors_spectral$values, col=z_colors_spectral$colors)
 
 p3 <- levelplot(rankr.msnfi.abf.pe.w, FUN.margin=median, 
-                par.settings=rasterTheme(region=z_colors_spectral$colors))
+                maxpixels=1e6,
+                par.settings=rasterTheme(region=z_colors_spectral$colors),
+                at=z_colors_spectral$values, colorkey=z_color_key)
 p4 <- levelplot(rankr.msnfi.abf.pe.w.cmat, FUN.margin=median, 
-                par.settings=rasterTheme(region=z_colors_spectral$colors))
+                maxpixels=1e6,
+                par.settings=rasterTheme(region=z_colors_spectral$colors),
+                at=z_colors_spectral$values, colorkey=z_color_key)
 
 # All data ----------------------------------------------------------------
   
@@ -38,6 +50,10 @@ rankr.abf.pe.w.cmat <- rank_raster(abf.pe.w.cmat)
 #     breaks=z_colors_spectral$values, col=z_colors_spectral$colors)
 
 p5 <- levelplot(rankr.abf.pe.w, FUN.margin=median, 
-                par.settings=rasterTheme(region=z_colors_spectral$colors))
+                maxpixels=1e6,
+                par.settings=rasterTheme(region=z_colors_spectral$colors),
+                at=z_colors_spectral$values, colorkey=z_color_key)
 p6 <- levelplot(rankr.abf.pe.w.cmat, FUN.margin=median, 
-                par.settings=rasterTheme(region=z_colors_spectral$colors))
+                maxpixels=1e6,
+                par.settings=rasterTheme(region=z_colors_spectral$colors),
+                at=z_colors_spectral$values, colorkey=z_color_key)
