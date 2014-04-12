a8e794666b957d8be8aad6df9120b8a35db541bb
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-12 17:31:43 +0300

    Add loading comparison to Fig4

--- src/Figure4.R
+++ src/Figure4.R
@@ -64,6 +64,7 @@ cur.nosfc.msnfi.abf.pe.w.cmat <- curves(nosfc.msnfi.abf.pe.w.cmat)
 # Pre-loaded
 
 grpcur.loaded.abf.pe.w <- curves(loaded.abf.pe.w, groups=TRUE)
+grpcur.loaded.abf.pe.w.cmat <- curves(loaded.abf.pe.w.cmat, groups=TRUE)
 
 # Plotting ----------------------------------------------------------------
 
@@ -94,6 +95,33 @@ p5 <- plot(cur.nosfc.msnfi.abf.pe.w, monochrome=FALSE, min=FALSE, mean=FALSE,
 p5 <- p5  + ggtitle("MSNFI data abf_pe_w by soil fertility class")
 p6 <- plot(cur.nosfc.msnfi.abf.pe.w.cmat, monochrome=FALSE, min=FALSE, 
            mean=FALSE, invert.x=TRUE) + ylim(0, 1)
-p6 <- p6  + ggtitle("MSNFI data abf_pe_w by soil fertility class")
+p6 <- p6  + ggtitle("MSNFI data abf_pe_w by Psoil fertility class")
 
 grid.arrange(p1, p2, p3, p4, p5, p6, nrow=3)
+
+# Performance differences -------------------------------------------------
+
+# Calculate the difference in performance levels between the analysis based on
+# the detailed data and on the pre-loaded version (detailed data forced to the
+# rank order of analysis based on MSNFI with classes)
+
+# dd = detailed data, Full
+
+# Get just the mean columns
+dd <- grpcur.abf.pe.w[,c(1, seq(4, ncol(grpcur.abf.pe.w), 5))]
+m.dd <- melt(dd, id.vars=c("pr_lost"))
+m.dd$type <- "Full"
+
+loaded <- grpcur.loaded.abf.pe.w[,c(1, seq(4, ncol(grpcur.loaded.abf.pe.w), 5))]
+m.loaded <- melt(loaded, id.vars=c("pr_lost"))
+m.loaded$type <- "Loaded"
+
+dat <- rbind(m.dd, m.loaded)
+dat$variable <- gsub("^mean\\.", "", dat$variable)
+
+p7 <- ggplot(dat, aes(x=pr_lost, y=value, color=type)) 
+p7 + geom_line(size=1.25) + facet_wrap(~variable) +
+  ylab("Prop. of distributions remaining\n") +
+  scale_x_continuous(breaks=seq(0, 1, 0.2), 
+                     labels=c("1.0", "0.8", "0.6", "0.4", "0.2", "0.0")) + 
+  xlab("Prop. of landscape under conservation")
