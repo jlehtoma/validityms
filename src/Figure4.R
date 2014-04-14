782587e9d78e0c25a0dbbff6c6d0d41a5411ce73
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-14 19:12:51 +0300

    Refactor Fig4 to use new variant names

--- src/Figure4.R
+++ src/Figure4.R
@@ -12,96 +12,91 @@ con.fert.labels <- c("1"="Herb-rich-con", "2"="Herb-rich-like-con",
                      "9"="Semi-xeric", "10"="Xeric")
 
 # Calculate new grouping based on soil fertility
-groups(abf.pe.w) <- rep(1:5, 4)
-groupnames(abf.pe.w) <- fert.labels
+groups(V1) <- rep(1:5, 4)
+groupnames(V1) <- fert.labels
 
-groups(msnfi.abf.pe.w) <- rep(1:5, 4)
-groupnames(msnfi.abf.pe.w) <- fert.labels
+groups(V3) <- rep(1:5, 4)
+groupnames(V3) <- fert.labels
 
-groups(msnfi.loaded.abf.pe.w) <- rep(1:5, 4)
-groupnames(msnfi.loaded.abf.pe.w) <- fert.labels
+groups(V1.load.V3) <- rep(1:5, 4)
+groupnames(V1.load.V3) <- fert.labels
 
-groups(nosfc.msnfi.loaded.abf.pe.w) <- rep(1:5, 4)
-groupnames(nosfc.msnfi.loaded.abf.pe.w) <- fert.labels
+groups(V1.load.V5) <- rep(1:5, 4)
+groupnames(V1.load.V5) <- fert.labels
 
 # When assigning new groups to the connectivity transformed variant, remember
 # that the feature stack is duplicated: 1st for connectivity transformations, 
 # 2nd time for local quality. Labels need to be reconstructed as well.
-groups(abf.pe.w.cmat) <- c(rep(1:5, 4), rep(6:10, 4))
-groupnames(abf.pe.w.cmat) <- con.fert.labels
+groups(V2) <- c(rep(1:5, 4), rep(6:10, 4))
+groupnames(V2) <- con.fert.labels
+
+groups(V4) <- c(rep(1:5, 4), rep(6:10, 4))
+groupnames(V4) <- con.fert.labels
 
 # Performance curves by soil fertility --------------------------------------
 
 # Detailed data
 
-grpcur.abf.pe.w <- curves(abf.pe.w, groups=TRUE)
+grpcur.V1 <- curves(V1, groups=TRUE)
 # Get columns 21:40 -> these are NOT transformed by matrix connectivity so 
 # they describe local quality. cols can't be used in curves() when groups=TRUE,
 # so the group curves data frame has to be manually divided.
-grpcur.abf.pe.w.cmat <- curves(abf.pe.w.cmat, groups=TRUE)
+grpcur.V2 <- curves(V2, groups=TRUE)
 # Get only columns that don't end with "-con" (for connectivity)
-nocon.grpcur.abf.pe.w.cmat <- grpcur.abf.pe.w.cmat[,-grep("-con$", names(grpcur.abf.pe.w.cmat))]
+nocon.grpcur.V2 <- grpcur.V2[,-grep("-con$", names(grpcur.V2))]
 # We'll have to manually transform this back ZGroupCurvesDataFrame
-nocon.grpcur.abf.pe.w.cmat <- new("ZGroupCurvesDataFrame", 
-                                  nocon.grpcur.abf.pe.w.cmat)
+nocon.grpcur.V2 <- new("ZGroupCurvesDataFrame", nocon.grpcur.V2)
 
 # MSNFI-classes data
 
-grpcur.msnfi.abf.pe.w <- curves(msnfi.abf.pe.w, groups=TRUE)
+grpcur.V3 <- curves(V3, groups=TRUE)
 # Get columns 21:40 -> these are NOT transformed by matrix connectivity so 
 # they describe local quality. cols can't be used in curves() when groups=TRUE,
 # so the group curves data frame has to be manually divided.
-grpcur.msnfi.abf.pe.w.cmat <- curves(msnfi.abf.pe.w.cmat, groups=TRUE)
+grpcur.V4 <- curves(V4, groups=TRUE)
 # Get only columns that don't end with "-con" (for connectivity)
-nocon.grpcur.msnfi.abf.pe.w.cmat <- grpcur.msnfi.abf.pe.w.cmat[,-grep("-con$", names(grpcur.msnfi.abf.pe.w.cmat))]
+nocon.grpcur.V4 <- grpcur.V4[,-grep("-con$", names(grpcur.V4))]
 # We'll have to manually transform this back ZGroupCurvesDataFrame
-nocon.grpcur.msnfi.abf.pe.w.cmat <- new("ZGroupCurvesDataFrame", 
-                                        nocon.grpcur.msnfi.abf.pe.w.cmat)
+nocon.grpcur.V4 <- new("ZGroupCurvesDataFrame", nocon.grpcur.V4)
 
 # MSNFI data
 
 # No groups used/available
-cur.nosfc.msnfi.abf.pe.w <- curves(nosfc.msnfi.abf.pe.w)
-cur.nosfc.msnfi.abf.pe.w.cmat <- curves(nosfc.msnfi.abf.pe.w.cmat)
+cur.V5 <- curves(V5)
+cur.V6 <- curves(V6)
 
 # Pre-load MSNFI / MSNFI-nosfc ranking
 
-grpcur.msnfi.loaded.abf.pe.w <- curves(msnfi.loaded.abf.pe.w, groups=TRUE)
-grpcur.msnfi.loaded.abf.pe.w.cmat <- curves(msnfi.loaded.abf.pe.w.cmat, 
-                                            groups=TRUE)
+grpcur.V1.load.V3 <- curves(V1.load.V3, groups=TRUE)
+grpcur.V2.load.V4 <- curves(V2.load.V4, groups=TRUE)
 
-grpcur.nosfc.msnfi.loaded.abf.pe.w <- curves(nosfc.msnfi.loaded.abf.pe.w, groups=TRUE)
-grpcur.nosfc.msnfi.loaded.abf.pe.w.cmat <- curves(nosfc.msnfi.loaded.abf.pe.w.cmat, 
-                                            groups=TRUE)
+grpcur.V1.load.V5 <- curves(V1.load.V5, groups=TRUE)
+grpcur.V2.load.V6 <- curves(V2.load.V6, groups=TRUE)
 
 # Plotting ----------------------------------------------------------------
 
-p1 <- plot(grpcur.abf.pe.w, monochrome=FALSE, min=FALSE, mean=FALSE, max=FALSE,
+p1 <- plot(grpcur.V1, monochrome=FALSE, min=FALSE, mean=FALSE, max=FALSE,
            invert.x=TRUE) + ylim(0, 1)
 p1 <- p1 + ggtitle("Detailed data abf_pe_w by soil fertility class")
 
-p1B <- plot(grpcur.loaded.abf.pe.w, monochrome=FALSE, min=FALSE, mean=FALSE, max=FALSE,
-           invert.x=TRUE) + ylim(0, 1)
-p1B <- p1B + ggtitle("Pre-loaded data abf_pe_w by soil fertility class")
-
-p2 <- plot(nocon.grpcur.abf.pe.w.cmat, monochrome=FALSE, min=FALSE, mean=FALSE, 
+p2 <- plot(nocon.grpcur.V2, monochrome=FALSE, min=FALSE, mean=FALSE, 
            max=FALSE, invert.x=TRUE)
 p2 <- p2 + ylim(0, 1) + 
   ggtitle("Detailed data abf_pe_w_cmat by soil fertility class (local quality)")
 
-p3 <- plot(grpcur.abf.pe.w, monochrome=FALSE, min=FALSE, mean=FALSE, max=FALSE,
+p3 <- plot(grpcur.V3, monochrome=FALSE, min=FALSE, mean=FALSE, max=FALSE,
            invert.x=TRUE) + ylim(0, 1)
 p3 <- p3 + ggtitle("MSNFI-classes data abf_pe_w by soil fertility class")
 
-p4 <- plot(nocon.grpcur.abf.pe.w.cmat, monochrome=FALSE, min=FALSE, mean=FALSE, 
+p4 <- plot(nocon.grpcur.V4, monochrome=FALSE, min=FALSE, mean=FALSE, 
            max=FALSE, invert.x=TRUE)
 p4 <- p4 + ylim(0, 1) + 
   ggtitle("MSNFI-classes data abf_pe_w_cmat by soil fertility class (local quality)")
 
-p5 <- plot(cur.nosfc.msnfi.abf.pe.w, monochrome=FALSE, min=FALSE, mean=FALSE, 
+p5 <- plot(cur.V5, monochrome=FALSE, min=FALSE, mean=FALSE, 
            invert.x=TRUE) + ylim(0, 1)
 p5 <- p5  + ggtitle("MSNFI data abf_pe_w by soil fertility class")
-p6 <- plot(cur.nosfc.msnfi.abf.pe.w.cmat, monochrome=FALSE, min=FALSE, 
+p6 <- plot(cur.V6, monochrome=FALSE, min=FALSE, 
            mean=FALSE, invert.x=TRUE) + ylim(0, 1)
 p6 <- p6  + ggtitle("MSNFI data abf_pe_w by Psoil fertility class")
 
@@ -116,15 +111,22 @@ grid.arrange(p1, p2, p3, p4, p5, p6, nrow=3)
 # dd = detailed data, Full
 
 # Get just the mean columns
-dd <- grpcur.abf.pe.w[,c(1, seq(4, ncol(grpcur.abf.pe.w), 5))]
+dd <- grpcur.V1[,c(1, seq(4, ncol(grpcur.V1), 5))]
 m.dd <- melt(dd, id.vars=c("pr_lost"))
-m.dd$type <- "Full"
+m.dd$type <- "Detailed data"
 
-loaded.msnfi <- grpcur.msnfi.loaded.abf.pe.w[,c(1, seq(4, ncol(grpcur.loaded.abf.pe.w), 5))]
-m.loaded <- melt(loaded, id.vars=c("pr_lost"))
-m.loaded$type <- "Loaded"
+loaded.msnfi.sfc <- grpcur.V1.load.V3[,c(1, seq(4, ncol(grpcur.V1.load.V3), 5))]
+m.loaded.msnfi.sfc <- melt(loaded.msnfi.sfc, id.vars=c("pr_lost"))
+m.loaded.msnfi.sfc$type <- "Loaded msnfi-sfc"
 
-dat <- rbind(m.dd, m.loaded)
+loaded.msnfi <- grpcur.V1.load.V5[,c(1, seq(4, ncol(grpcur.V1.load.V5), 5))]
+m.loaded.msnfi <- melt(loaded.msnfi, id.vars=c("pr_lost"))
+m.loaded.msnfi$type <- "Loaded msnfi"
+
+
+dat <- do.call("rbind", list("A"=m.dd, "B"=m.loaded.msnfi.sfc, 
+                             "C"=m.loaded.msnfi))
+
 dat$variable <- gsub("^mean\\.", "", dat$variable)
 
 p7 <- ggplot(dat, aes(x=pr_lost, y=value, color=type)) 
