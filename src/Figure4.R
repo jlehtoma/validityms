66459fc1117a249eb9ebe475b79fcc52e6d832ca
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-14 19:28:14 +0300

    Add plain MSNFI to load-Fig4

--- src/Figure4.R
+++ src/Figure4.R
@@ -109,20 +109,30 @@ grid.arrange(p1, p2, p3, p4, p5, p6, nrow=3)
 # rank order of analysis based on MSNFI with classes)
 
 # dd = detailed data, Full
-
 # Get just the mean columns
 dd <- grpcur.V1[,c(1, seq(4, ncol(grpcur.V1), 5))]
 m.dd <- melt(dd, id.vars=c("pr_lost"))
+# Get the average over everything
+dd.all <- data.frame(pr_lost=V1@results@curves$pr_lost, variable="mean.All",
+                     value=V1@results@curves$ave_pr)
+m.dd <- rbind(m.dd, dd.all)
 m.dd$type <- "Detailed data"
 
 loaded.msnfi.sfc <- grpcur.V1.load.V3[,c(1, seq(4, ncol(grpcur.V1.load.V3), 5))]
 m.loaded.msnfi.sfc <- melt(loaded.msnfi.sfc, id.vars=c("pr_lost"))
+m.loaded.msnfi.sfc.all <- data.frame(pr_lost=V1.load.V3@results@curves$pr_lost, 
+                                     variable="mean.All",
+                                     value=V1.load.V3@results@curves$ave_pr)
+m.loaded.msnfi.sfc <- rbind(m.loaded.msnfi.sfc, m.loaded.msnfi.sfc.all)
 m.loaded.msnfi.sfc$type <- "Loaded msnfi-sfc"
 
 loaded.msnfi <- grpcur.V1.load.V5[,c(1, seq(4, ncol(grpcur.V1.load.V5), 5))]
 m.loaded.msnfi <- melt(loaded.msnfi, id.vars=c("pr_lost"))
+m.loaded.msnfi.all <- data.frame(pr_lost=V1.load.V5@results@curves$pr_lost, 
+                                     variable="mean.All",
+                                     value=V1.load.V5@results@curves$ave_pr)
+m.loaded.msnfi <- rbind(m.loaded.msnfi, m.loaded.msnfi.all)
 m.loaded.msnfi$type <- "Loaded msnfi"
-
 
 dat <- do.call("rbind", list("A"=m.dd, "B"=m.loaded.msnfi.sfc, 
                              "C"=m.loaded.msnfi))
