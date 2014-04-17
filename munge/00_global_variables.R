40cffa718814e4e9a42f24bb31926331b0f8b158
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-17 12:12:26 +0300

    Move global variables to munging file

---
+++ munge/00_global_variables.R
@@ -0,0 +1,9 @@
+# Set the plot labels
+spp.labels <- c("1"="Birch", "2"="Spruce", "3"="OtherDec", "4"="Pine")
+fert.labels <- c("1"="Herb-rich", "2"="Herb-rich-like", "3"="Mesic", 
+                 "4"="Semi-xeric", "5"="Xeric")
+con.fert.labels <- c("1"="Herb-rich-con", "2"="Herb-rich-like-con", 
+                     "3"="Mesic-con", "4"="Semi-xeric-con", "5"="Xeric-con",
+                     "6"="Herb-rich", "7"="Herb-rich-like", "8"="Mesic", 
+                     "9"="Semi-xeric", "10"="Xeric")
+
