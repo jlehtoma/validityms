1d38e6858c8932698b11d70ef7fe8fa128154fe6
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-26 14:10:11 +0300

    Add figure and front matter back

--- src/Figure5.R
+++ src/Figure5.R
@@ -3,6 +3,8 @@ load.project()
 
 # Re-name groups ----------------------------------------------------------
 
+# Local quality
+
 groups(V5) <- rep(1:5, 4)
 groupnames(V5) <- fert.labels
 
@@ -12,6 +14,16 @@ groupnames(V5.load.V3) <- fert.labels
 groups(V5.load.V1) <- rep(1:5, 4)
 groupnames(V5.load.V1) <- fert.labels
 
+# Connectivity
+
+groups(V6) <- c(rep(1:5, 4), rep(6:10, 4))
+groupnames(V6) <- con.fert.labels
+
+groups(V6.load.V2) <- c(rep(1:5, 4), rep(6:10, 4))
+groupnames(V6.load.V2) <- con.fert.labels
+
+groups(V6.load.V4) <- c(rep(1:5, 4), rep(6:10, 4))
+groupnames(V6.load.V4) <- con.fert.labels
 
 # Get curves data ---------------------------------------------------------
 
@@ -31,7 +43,8 @@ grpcur.V6.load.V4 <- curves(V6.load.V4, groups=TRUE)
 # the detailed data and on the pre-loaded version (detailed data forced to the
 # rank order of analysis based on MSNFI with classes)
 
-# dd = detailed data, Full
+# Just the local quality
+
 # Get just the mean columns
 dd <- grpcur.V5[,c(1, seq(4, ncol(grpcur.V5), 5))]
 m.dd <- melt(dd, id.vars=c("pr_lost"))
@@ -39,7 +52,7 @@ m.dd <- melt(dd, id.vars=c("pr_lost"))
 dd.all <- data.frame(pr_lost=V1@results@curves$pr_lost, variable="mean.All",
                      value=V1@results@curves$ave_pr)
 m.dd <- rbind(m.dd, dd.all)
-m.dd$type <- "Detailed data"
+m.dd$type <- "V5"
 
 loaded.msnfi.sfc <- grpcur.V5.load.V3[,c(1, seq(4, ncol(grpcur.V5.load.V3), 5))]
 m.loaded.msnfi.sfc <- melt(loaded.msnfi.sfc, id.vars=c("pr_lost"))
@@ -47,7 +60,7 @@ m.loaded.msnfi.sfc.all <- data.frame(pr_lost=V5.load.V3@results@curves$pr_lost,
                                      variable="mean.All",
                                      value=V5.load.V3@results@curves$ave_pr)
 m.loaded.msnfi.sfc <- rbind(m.loaded.msnfi.sfc, m.loaded.msnfi.sfc.all)
-m.loaded.msnfi.sfc$type <- "Loaded msnfi-sfc"
+m.loaded.msnfi.sfc$type <- "V3"
 
 loaded.msnfi <- grpcur.V5.load.V1[,c(1, seq(4, ncol(grpcur.V5.load.V1), 5))]
 m.loaded.msnfi <- melt(loaded.msnfi, id.vars=c("pr_lost"))
@@ -55,19 +68,26 @@ m.loaded.msnfi.all <- data.frame(pr_lost=V5.load.V1@results@curves$pr_lost,
                                  variable="mean.All",
                                  value=V5.load.V1@results@curves$ave_pr)
 m.loaded.msnfi <- rbind(m.loaded.msnfi, m.loaded.msnfi.all)
-m.loaded.msnfi$type <- "Loaded msnfi"
+m.loaded.msnfi$type <- "V1"
 
 dat <- do.call("rbind", list("A"=m.dd, "B"=m.loaded.msnfi.sfc, 
                              "C"=m.loaded.msnfi))
 
 dat$variable <- gsub("^mean\\.", "", dat$variable)
 
-p1 <- ggplot(dat, aes(x=pr_lost, y=value, color=type)) 
-p1 <- p1 + geom_line(size=1.25) + facet_wrap(~variable) +
+dat$lt <- 1
+dat$lt[which(dat$type == "V1")] <- 2
+dat$lt[which(dat$type == "V3")] <- 3
+dat$lt <- as.factor(dat$lt)
+
+p1 <- ggplot(dat, aes(x=pr_lost, y=value, linetype=lt)) 
+p1 <- p1 + geom_line(size=0.8) + facet_wrap(~variable) +
   ylab("Prop. of distributions remaining\n") +
   scale_x_continuous(breaks=seq(0, 1, 0.2), 
                      labels=c("1.0", "0.8", "0.6", "0.4", "0.2", "0.0")) + 
-  xlab("Prop. of landscape under conservation")
+  xlab("Prop. of landscape under conservation") + 
+  scale_linetype_discrete(name="Ranking from",
+                          labels=c("V5", "V1", "V3")) + theme_bw()
 
 png(file="figs/Figure5/Fig5.png", width=1000, height=800)
 p1
---
+++ validity_ms_front_matter.md
@@ -0,0 +1,29 @@
+# Assessing the suitability of  forest inventories for basis of  spatial conservation prioritization
+
+Joona Lehtomäki^1,2*^ Sakari Tuominen^3^ and Antti Leinonen^4^
+
+1 Department of Biosciences, P.O. Box 65 (Viikinkaari 1), FI-00014 University of Helsinki, Finland  
+2 Finnish Environment Institute, Natural Environment Centre, P.O. Box 140 (Mechelininkatu 34a), FI- 00251 Helsinki, Finland  
+3 Finnish Forest Research Institute (Metsäntutkimuslaitos), Vantaa, Finland  
+4 The Finnish Forest Centre (Suomen Metsäkeskus), Kajaani, Finland  
+* Corresponding author  
+
+**Contact:**  
+joona.lehtomaki@helsinki.fi, tel. +358-9-191-57714  
+sakari.tuominen@metla.fi  
+antti.leinonen@metsakeskus.fi  
+
+**Journal:**  
+Scandinavian Journal of Forest Research  
+
+**Type of paper:**  
+Original research paper
+
+**Running title:**  
+Validation of spatial conservation prioritization
+
+**Manuscript statistics:**  
+* word count (total) = xxx  
+* figures = xxx of which xxx in color  
+* tables = xxx  
+* references = xxx  
