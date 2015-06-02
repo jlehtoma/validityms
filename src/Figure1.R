f14f85deb174eeab390cb3a96160de2ae6768310
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-06-02 21:14:17 +0300

    Adjust data plots

--- src/Figure1.R
+++ src/Figure1.R
@@ -12,11 +12,7 @@ fin.0.wgs84 <- getData('GADM', country='FIN', level=0, path="cache")
 fin.0.kkj <- spTransform(fin.0.wgs84, "+init=epsg:2393")
 
 fin.2.wgs84 <- getData('GADM', country='FIN', level=2, path="cache")
-fin.2.kkj <- spTransform(fin.wgs84, "+init=epsg:2393")
-
-# Grab just Southern Savonia
-ssavonia.kkj <- subset(fin.2.kkj, NAME_2 == "Southern Savonia") 
-
+fin.2.kkj <- spTransform(fin.2.wgs84, "+init=epsg:2393")
 
 # Data source -------------------------------------------------------------
 
@@ -26,9 +22,34 @@ ds_rat <- levels(ds.mask)[[1]]
 ds_rat[["data_source"]] <- c("FPS", "FFC", "MS-NFI")
 levels(ds.mask) <- ds_rat
 
-ds_cols <- brewer.pal(3, "Set2")
-levelplot(ds.mask, col.regions=ds_cols, xlab="", ylab="", maxpixels=1e6)
+ds_cols <- brewer.pal(3, "Dark2")
 
+png(file="figs/Figure1/levelplots/Fig1_data_sources.png", width=950, 
+    height=950)
+
+levelplot(ds.mask, col.regions=ds_cols, xlab="", ylab="", maxpixels=1e6) +
+  latticeExtra::layer(sp.polygons(esmk.mask))
+
+dev.off()
+
+
+# Soil fertility ----------------------------------------------------------
+
+sfc.mask <- as.factor(sfc.mask)
+sfc_rat <- levels(sfc.mask)[[1]]
+sfc_rat[["soil_fertility_class"]] <- c("herb-rich", "herb-rich like", "mesic", 
+                                       "semi-xeric", "xeric")
+levels(sfc.mask) <- sfc_rat
+
+sfc_cols <- rev(brewer.pal(5, "BrBG"))
+
+png(file="figs/Figure1/levelplots/Fig1_soil_fertility_class.png", width=950, 
+    height=950)
+
+levelplot(sfc.mask, col.regions=sfc_cols, xlab="", ylab="", maxpixels=1e6) +
+  latticeExtra::layer(sp.polygons(esmk.mask))
+
+dev.off()
 
 # Masks -------------------------------------------------------------------
 
@@ -53,5 +74,12 @@ masks_rat <- levels(masks)[[1]]
 masks_rat[["type"]] <- c("PA", "WKH", "METSO")
 levels(masks) <- masks_rat
 
-masks_cols <- brewer.pal(3, "Set2")
-levelplot(masks, col.regions=ds_cols, xlab="", ylab="", maxpixels=1e6)
+masks_cols <- brewer.pal(3, "Dark2")
+
+png(file="figs/Figure1/levelplots/Fig1_validation_data.png", width=950, 
+    height=950)
+
+levelplot(masks, col.regions=masks_cols, xlab="", ylab="", maxpixels=1e6) +
+  latticeExtra::layer(sp.polygons(esmk.mask))
+
+dev.off()
