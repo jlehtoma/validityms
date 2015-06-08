548f84589db967db291caa9f70611585a9454310
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-06-08 13:20:41 +0300

    Update Fig 3

--- figs/Figure4/levelplots/crop_marginal_plots.py
+++ figs/Figure4/levelplots/crop_marginal_plots.py
@@ -3,15 +3,9 @@
 from PIL import Image
 import glob, os
 
-# Define crop sizes in pixels
-x_crop = 75
-y_crop = 75
 
 # Where to save the marginal plots
 output_folder = "marginal_plots"
-
-# Define how much needs to be cropped in order to get rid of the legend
-legend_crop = 760
 
 for infile in glob.glob("p*.png"):
     
@@ -23,19 +17,10 @@ for infile in glob.glob("p*.png"):
     
     # Original image size
     width, height = im.size
-    # First, crop off the legend
-    im_noleg = im.crop((0, 0, width, legend_crop))
-    #im.save(filename + "_noleg.png")
 
-    # Process both x and y marginal plots
-    
-    # Update image size
-    width, height = im_noleg.size
-
-    im_x_plot = im_noleg.crop((0, 0, width, x_crop))
-    im_y_plot = im_noleg.crop((width-y_crop, 0, width, height)) 
+    im_x_plot = im.crop((0, 0, 752, 125))
+    im_y_plot = im.crop((752, 118, width, height)) 
     
     # Save the cropped images
-    im_noleg.save(prefix + ".png")
     im_x_plot.save(os.path.join(output_folder, prefix + "_xplot.png"))
     im_y_plot.save(os.path.join(output_folder, prefix + "_yplot.png"))
